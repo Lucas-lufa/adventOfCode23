285be3305a9ee1314f76469733b4d50bc8221e44
Lucas <lucasfad@hotmail.com>
2023-12-23 02:31:27 +0800

    Day three

--- dayTwo.py
+++ dayTwo.py
@@ -11,7 +11,6 @@ def findFirstNumber(line:str):
         if character.isdigit():
             return int(character)
 
-
 def dayTwo():
     count = 0
     with open(textFile, 'r') as file:
@@ -38,9 +37,47 @@ def dayTwo():
 
     print(count)
 
+def theMost(character,rgb,index):
+    dice = int(character)
+    if rgb[index] < dice:
+        rgb[index] = dice
+    return ''
+
+def dayTwoPartTwo():
+    """  """
+    sumList = []
+    with open(textFile, 'r') as file:
+        for line in file:
+            rgb = [0,0,0]
+            character = ''
+            for i in line:
+                if i.isdigit():
+                    character +=i
+                    continue
+                if character.isdigit():
+                    match i:
+                        case ':':
+                            character = ''
+                        case 'r':
+                            index = 0
+                            character = theMost(character,rgb,index)
+                        case 'g':
+                            index = 1
+                            character = theMost(character,rgb,index)
+                        case 'b':
+                            index = 2
+                            character = theMost(character,rgb,index)
+
+            sumList.append(rgb[0]*rgb[1]*rgb[2])
+    print(sum(sumList))
+                
+
+
+
+
 
 def main():
     """ """
-    dayTwo()
+    dayTwoPartTwo()
 if __name__ == '__main__':
     main()
